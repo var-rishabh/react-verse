5d402d426945af2ff83ed8b6921338c0cbe04def
Rishabh Varshney <right.rishabh@gmail.com>
2023-09-12 00:41:17 +0530

    tic-tac-toe finished ✅

--- tic-tac-toe/src/components/Footer.jsx
+++ tic-tac-toe/src/components/Footer.jsx
@@ -3,9 +3,12 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <footer className="footer">
-            Made by 
-                <a href="http://rishabhvarshney.co" target="blank"> Radioactive </a>
-            with &#10084;
+            Made by &nbsp;
+            <a href="http://rishabhvarshney.co" className="a" target="blank">
+                {" "}
+                Radioactive{" "}
+            </a>
+            &nbsp; with &#10084;
         </footer>
     );
 };
